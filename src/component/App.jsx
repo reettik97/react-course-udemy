dd7f0ce2248a2a8f78c61590c8f7642723f03ea2
reettik <reettikgoswami1997@gmail.com>
2020-05-17 15:58:29 +0530

    song app done

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,51 +1,18 @@
 import React, { Component } from "react";
 
-import SearchBar from "./SearchBar";
-import youtude from "../api/youtube";
-import VideoDetail from "./VideoDetail.jsx";
-import VideoList from "./VideoList";
-
-const KEY = "";
+import SongDetail from "./SongDetail";
+import SongList from "./SongList";
 
 class App extends Component {
-  state = { videos: [], selectedVideo: null };
-
-componentDidMount() {
-  this.onSearchTerm('funny video');
-}
-
-
-  onSearchTerm = async (term) => {
-    const response = await youtude.get("/search", {
-      params: {
-        part: "snippet",
-        maxResults: 5,
-        key: KEY,  // google api key
-        q: term,
-      },
-    });
-    this.setState({ videos: response.data.items , selectedVideo : response.data.items[0] });
-  };
-
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-
   render() {
     return (
-      <div className="ui container" style={{ marginTop: "20px" }}>
-        <SearchBar onFormSubmit={this.onSearchTerm} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+      <div className="ui container grid" style={{marginTop: "40px"}}>
+        <div className="row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className = "column eight wide">
+            <SongDetail/>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import reducer from "./reducers/index";
 import App from "./component/App";
 
-
-ReactDOM.render(<App/> , document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store = {createStore(reducer)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
